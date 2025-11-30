8360a9c13f70acdae780a1d428c0114290203c46
Nematullah <imamsanghaar@gmail.com>
2025-11-30 16:46:18 +0500

    Fix gitignore and remove tracked build directories

--- .docusaurus/client-modules.js
+++
@@ -1,7 +0,0 @@
-export default [
-  require("D:\\piaic_hackathon\\my-book\\.docusaurus\\docusaurus-plugin-css-cascade-layers\\default\\layers.css"),
-  require("D:\\piaic_hackathon\\my-book\\node_modules\\infima\\dist\\css\\default\\default.css"),
-  require("D:\\piaic_hackathon\\my-book\\node_modules\\@docusaurus\\theme-classic\\lib\\prism-include-languages"),
-  require("D:\\piaic_hackathon\\my-book\\node_modules\\@docusaurus\\theme-classic\\lib\\nprogress"),
-  require("D:\\piaic_hackathon\\my-book\\src\\css\\custom.css"),
-];
